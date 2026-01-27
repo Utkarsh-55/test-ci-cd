1110d1d1a6343f27e6785d4df1860ed90843ca4e
utkarsh-payal <utkarsh.payal@fiftyfivetech.io>
2026-01-27 18:33:27 +0530

    fix: this commit adds docker file

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+const PORT = 8080;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+app.listen(8080, '0.0.0.0', () => {
+  console.log('Running on http://0.0.0.0:8080');
+});
